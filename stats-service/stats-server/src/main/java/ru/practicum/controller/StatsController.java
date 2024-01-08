a1c436b5a080413480864f6880d08fd6b172728e
Alexandr <118847574+kosalexkos@users.noreply.github.com>
2024-01-08 23:24:37 +0300

    Fixing Stat Issues
    Fixed the response issues in stat server

--- stats-service/stats-server/src/main/java/ru/practicum/controller/StatsController.java
+++ stats-service/stats-server/src/main/java/ru/practicum/controller/StatsController.java
@@ -1,38 +1,37 @@
 package ru.practicum.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.EndpointHitDto;
 import ru.practicum.ViewStats;
 import ru.practicum.service.StatsService;
 
+import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Slf4j
 @RestController
 @Validated
 @RequiredArgsConstructor
 public class StatsController {
-    private final StatsService service;
-    public final String dateTime = "yyyy-MM-dd HH:mm:ss";
+    public static final String DATE_TIME = "yyyy-MM-dd HH:mm:ss";
+    private final StatsService statsService;
 
     @PostMapping("/hit")
-    public void addHit(@RequestBody EndpointHitDto endpointHitDto) {
-        log.info("Request to add entry: {}", endpointHitDto.toString());
-        service.addHit(endpointHitDto);
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addHit(@Valid @RequestBody EndpointHitDto endpointHitDto) {
+        statsService.addHit(endpointHitDto);
     }
 
     @GetMapping("/stats")
     public List<ViewStats> getStats(
-            @RequestParam(name = "start") @DateTimeFormat(pattern = dateTime) LocalDateTime start,
-            @RequestParam(name = "end") @DateTimeFormat(pattern = dateTime) LocalDateTime end,
+            @RequestParam(name = "start", required = false) @DateTimeFormat(pattern = DATE_TIME) LocalDateTime start,
+            @RequestParam(name = "end", required = false) @DateTimeFormat(pattern = DATE_TIME) LocalDateTime end,
             @RequestParam(required = false) List<String> uris,
             @RequestParam(defaultValue = "false") boolean unique) {
-        log.info("Processing get stats request");
-        return service.getStats(start, end, uris, unique);
+        return statsService.getStats(start, end, uris, unique);
     }
 }
--- stats-service/stats-server/src/main/java/ru/practicum/service/StatsService.java
+++ stats-service/stats-server/src/main/java/ru/practicum/service/StatsService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.EndpointHitDto;
 import ru.practicum.ViewStats;
+import ru.practicum.exception.DateTimeException;
 import ru.practicum.mapper.EndpointHitMapper;
 import ru.practicum.model.EndpointHit;
 import ru.practicum.repository.StatsRepository;
@@ -26,7 +27,7 @@ public class StatsService {
     @Transactional(readOnly = true)
     public List<ViewStats> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique) {
         if (start == null || end == null || start.isAfter(end)) {
-            throw new RuntimeException("Invalid date range OR 'start' or 'end' is null");
+            throw new DateTimeException("Invalid date range OR 'start' or 'end' is null");
         }
         return (unique) ? statsRepository.findUniqueStats(start, end, uris) :
                 statsRepository.findStats(start, end, uris);
